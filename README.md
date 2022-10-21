2a82be9bd9722fc49ce0dc8be05759eadb73bff1
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-10-21 14:09:42 +0530

    Initial Template

---
+++ README.md
@@ -0,0 +1,7 @@
+# 🌌 Particle Galaxy
+
+3D webpage a galaxy of particles where user can interact with the particles using the cursor.
+
+## 🔎 References:
+
+- Tutorial - [youtube link](https://youtu.be/o_bEveIFfoM)
---
+++ src/js/app.js
@@ -0,0 +1,160 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import fragment from "./shader/fragment.glsl";
+import vertex from "./shader/vertex.glsl";
+import GUI from "lil-gui";
+import gsap from "gsap";
+
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0x000000, 1);
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    );
+
+    /* var frustumSize = 10;
+    var aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.OrthographicCamera(
+      (frustumSize * aspect) / -2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      -1000,
+      1000
+    ); */
+
+    this.camera.position.set(0, 0, 2);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
+
+    this.dracoLoader = new DRACOLoader();
+    this.dracoLoader.setDecoderPath(
+      "https://raw.githubusercontent.com/mrdoob/three.js/dev//examples/js/libs/draco/"
+    );
+    this.gltf = new GLTFLoader();
+    this.gltf.setDRACOLoader(this.dracoLoader);
+
+    this.isPlaying = true;
+
+    this.addObjects();
+    this.resize();
+    this.render();
+    this.setupResize();
+    this.settings();
+  }
+
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
+
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+
+    // image cover
+    this.imageAspect = 853 / 1280;
+    let a1, a2;
+    if (this.height / this.width > this.imageAspect) {
+      a1 = (this.width / this.height) * this.imageAspect;
+      a2 = 1;
+    } else {
+      a1 = 1;
+      a2 = this.height / this.width / this.imageAspect;
+    }
+
+    this.material.uniforms.resolution.value.x = this.width;
+    this.material.uniforms.resolution.value.y = this.height;
+    this.material.uniforms.resolution.value.z = a1;
+    this.material.uniforms.resolution.value.w = a2;
+
+    // (optional) cover with quad
+    /* const dist = this.camera.position.z;
+    const height = 1;
+    this.camera.fov = 2 * (180 / Math.PI) * Math.atan(height / (2 * dist));
+
+    if (this.width / this.height > 1) {
+      this.plane.scale.x = this.camera.aspect;
+    } else {
+      this.plane.scale.y = 1 / this.camera.aspect;
+    } */
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable",
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { value: 0 },
+        resolution: { value: new THREE.Vector4() },
+      },
+      // wireframe: true,
+      // transparent: true,
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+
+    this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.render();
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById("container"),
+});
